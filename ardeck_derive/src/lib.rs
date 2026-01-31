975a6e3d30f8aa4d48dcf0a55bceb9621ad5d6ea
akurakuu <kuuakura@gmail.com>
2026-01-31 20:45:52 +0900

    テストコード記述中

--- ardeck_derive/src/lib.rs
+++ ardeck_derive/src/lib.rs
@@ -9,9 +9,42 @@ pub fn store_derive(input: TokenStream) -> TokenStream {
     let name = &ast.ident;
 
     let quote = quote! {
-        // use ardeck::store::StoreTrait;
-        // impl Store Trait for #name {}
+        impl ardeck::store::StoreTrait for #name {}
     };
 
     TokenStream::from(quote)
+}
+
+#[cfg(test)]
+mod tests {
+    use ardeck::{config::ConfigFile, store::StoreBuilder};
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Debug, Serialize, Deserialize, Clone)]
+    struct MyConfig {
+        name: String,
+        age: u32,
+    }
+
+    impl Default for MyConfig {
+        fn default() -> Self {
+            Self {
+                name: "John Doe".into(),
+                age: 42,
+            }
+        }
+    }
+
+    impl ConfigFile for MyConfig {
+        fn name() -> &'static str {
+            "my_config.json"
+        }
+    }
+
+    #[test]
+    fn store_builder() {
+        StoreBuilder::default().path("./".into()).init();
+
+        // MyConfig::
+    }
 }
